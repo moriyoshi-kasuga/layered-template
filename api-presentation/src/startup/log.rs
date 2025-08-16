e7aa80de8bcb357614ace63c0aa20445495222ad
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-08-16 12:22:36 +0900

    refactor(log): simplify logger initialization for different environments

--- api-presentation/src/startup/log.rs
+++ api-presentation/src/startup/log.rs
@@ -1,14 +1,20 @@
 use tracing::level_filters::LevelFilter;
 
-#[cfg(feature = "prod")]
 pub fn init_log() {
+    if cfg!(feature = "prod") {
+        prod();
+    } else {
+        dev();
+    }
+}
+
+fn prod() {
     tracing_subscriber::fmt()
         .with_max_level(LevelFilter::INFO)
         .init();
 }
 
-#[cfg(not(feature = "prod"))]
-pub fn init_log() {
+fn dev() {
     tracing_subscriber::fmt()
         .with_max_level(LevelFilter::DEBUG)
         .init();
